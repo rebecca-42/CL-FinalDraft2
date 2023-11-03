6df9c172305b03cf9fd2902fdcb638ca531fc306
Rebecca Sweigart <143544823+rebecca-42@users.noreply.github.com>
2023-11-02 22:04:28 -0400

    Initial Commit

---
+++ nlite/nliteApp.swift
@@ -0,0 +1,17 @@
+//
+//  nliteApp.swift
+//  nlite
+//
+//  Created by Rebecca Sweigart on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct nliteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
